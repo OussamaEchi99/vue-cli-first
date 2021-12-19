47526410176f5e6f2dec58cf03d50d0a927de98c
OussamaEchi99 <oussama.echi99@gmail.com>
2021-12-19 16:46:27 +0100

    Finito

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="jumbotron">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .jumbotron{
+        width: 100%;
+        height:  300px;
+        background-image: url('https://videohive.img.customer.envatousercontent.com/files/151036429/Preview_Image.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=109197ebc35d5c6efd146cd8cd4550a8');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
+
+
+</style>
